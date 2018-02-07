83e6c8549c821afb5bae2e4754d448405eefcf43
senthilnadhan <senthilnadhanr@gmail.com>
2018-02-08 02:03:14 +0530

    PHP array functions

--- functions/array/array_pad.php
+++ functions/array/array_pad.php
@@ -1,24 +1,30 @@
-<HTML>
-<HEAD>
-<TITLE>array_pad</TITLE>
-</HEAD>
-<BODY>
+<html>
+<head>
+    <title>array_pad</title>
+</head>
+<body>
 <?php
-	//create test data
-	$data = array(1,2,3);
-echo "<pre>";print_r($data);
-	//add "start" to beginning of array
-	$data = array_pad($data, -6, "start");
-echo "<pre>";print_r($data);
-	//add "end" to end of array
-	$data = array_pad($data, 7, "end");
+echo "Syntax:";
+echo "array_pad(array,size,value)";
+echo "<br>";
 
-	echo "<pre>";print_r($data);
-	
-	foreach($data as $value)
-	{
-		print("$value<BR>\n");
-	}
+//create test data
+$data = array(1, 2, 3);
+echo "<pre>";
+print_r($data);
+//add "start" to beginning of array
+$data = array_pad($data, -6, "start");
+echo "<pre>";
+print_r($data);
+//add "end" to end of array
+$data = array_pad($data, 7, "end");
+
+echo "<pre>";
+print_r($data);
+
+foreach ($data as $value) {
+    print("$value<BR>\n");
+}
 ?>
-</BODY>
-</HTML>
+</body>
+</html>
